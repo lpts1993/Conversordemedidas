a3b1394b3f9a013ada39950a529601ccbd025c9d
Leslie Tolagasi <85919085+lpts1993@users.noreply.github.com>
2024-11-24 12:35:56 -0500

    Add files via upload

---
+++ CONVERTOR/VISTAS/Convertir.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CONVERTOR.VISTAS
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Convertir : ContentPage
+	{
+
+		double cm;
+		double m;
+
+		public Convertir ()
+		{
+			InitializeComponent ();
+		}
+
+		private void ccalcular()
+		{
+		cm= Convert.ToDouble( txtcm.Text);
+			m = cm / 100;
+            lblresultado.Text = m.ToString() + " m";
+
+		}
+
+		private void Validar()
+		{
+			if (!string.IsNullOrEmpty(txtcm.Text))
+			{
+				ccalcular();
+			}
+			else {
+				DisplayAlert("ERROR", "Ingrese una canntidad", "ok");
+					
+					}
+
+		}
+
+        private void btnvolver_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PopAsync();
+        }
+        private void btncalcular_Clicked(object sender, EventArgs e)
+        {
+          Validar();
+        }
+        
+    }
+}
---
+++ CONVERTOR/VISTAS/MenuPrincipal.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CONVERTOR.VISTAS
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MenuPrincipal : ContentPage
+    {
+        public MenuPrincipal()
+        {
+            InitializeComponent();
+        }
+        private void btniniciar_Clicked(object sender, EventArgs e)
+        { 
+            Navigation.PushAsync(new Convertir());
+        }
+    }
+}
